f383e550ac77e2d1698c0586eee3f6fbba4792e0
VladShi <g3rc@yandex.ru>
2025-03-01 02:10:08 +0300

    Initial commit
    - Added parent POM and module configurations for task-tracker project

---
+++ task-tracker-email-sender/src/main/java/com/tasktracker/emailsender/EmailSenderApplication.java
@@ -0,0 +1,11 @@
+package com.tasktracker.emailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSenderApplication.class, args);
+    }
+}
