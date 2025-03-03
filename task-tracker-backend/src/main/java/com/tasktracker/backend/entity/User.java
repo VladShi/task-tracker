3c49cbfa435cf5afeab96527c960b7bdfdcb4d09
VladShi <g3rc@yandex.ru>
2025-03-03 14:11:41 +0300

    feat(db): add user entity and corresponding database table
    - Added User entity with fields id, username, and password.
    - Added a new database changelog file to create the users table.
    - Updated the cumulative changelog to include the new changes.

---
+++ task-tracker-backend/src/main/java/com/tasktracker/backend/entity/User.java
@@ -0,0 +1,25 @@
+package com.tasktracker.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username",unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
