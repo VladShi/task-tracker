c3ddd782caa76336a5bc336451f5610f5583d379
VladShi <g3rc@yandex.ru>
2025-03-05 15:21:07 +0300

    fix: do not add stack trace for custom exception
    - Modified `UsernameAlreadyTakenException` to override
    `fillInStackTrace()` method to prevent unnecessary stack trace
    generation, improving performance and reducing memory usage.

--- task-tracker-backend/src/main/java/com/tasktracker/backend/exception/UsernameAlreadyTakenException.java
+++ task-tracker-backend/src/main/java/com/tasktracker/backend/exception/UsernameAlreadyTakenException.java
@@ -4,4 +4,9 @@ public class UsernameAlreadyTakenException extends RuntimeException {
     public UsernameAlreadyTakenException(String message) {
         super(message);
     }
+
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
 }
