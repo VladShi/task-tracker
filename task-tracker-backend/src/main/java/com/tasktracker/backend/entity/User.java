a2620495260f29948fdd3df629552a0c29add298
VladShi <g3rc@yandex.ru>
2025-03-06 23:03:15 +0300

    feat(db): add tasks table and entity
    - Added `tasks` table to the database schema
    - Created `UserTask` entity class
    - Updated changelog to include new table

--- task-tracker-backend/src/main/java/com/tasktracker/backend/entity/User.java
+++ task-tracker-backend/src/main/java/com/tasktracker/backend/entity/User.java
@@ -17,7 +17,7 @@ public class User {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "username",unique = true, nullable = false)
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
     @Column(name = "password", nullable = false)
---
+++ task-tracker-backend/src/main/java/com/tasktracker/backend/entity/UserTask.java
@@ -0,0 +1,39 @@
+package com.tasktracker.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tasks")
+@Setter
+@Getter
+@NoArgsConstructor
+public class UserTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "completed", nullable = false)
+    private boolean completed;
+
+    @Column(name = "completed_at")
+    private Instant completedAt;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
